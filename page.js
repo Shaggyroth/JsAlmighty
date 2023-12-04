f7409c6b9216e24f341d42a20dfeac98259f4b23
Krumla <marek.krumlovsky.jr@gmail.com>
2023-12-04 19:45:18 +0100

    skeleton for nickname output"

--- page.js
+++ page.js
@@ -1,4 +1,6 @@
 function changeColor() {
   const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-  document.getElementById("header").style.color = randomColor;
+  document.getElementById("colorChange").style.color = randomColor;
 }
+
+function generateNickname() {}
